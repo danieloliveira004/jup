0ce032ce941888016ba38ccc43f2577aff92edd1
danieloliveira004 <danielsilvaoliveira004@gmail.com>
2022-06-27 13:54:03 -0300

    projeto iniciado

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>JUSPSI</title>
+
+  <!-- BOOTSTRAP CSS -->
+  <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
